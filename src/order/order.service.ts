af88eb08b71af045cb5e1856d5b4ef2108f7bbaa
ziclari <citlalli688@outlook.com>
2025-08-29 15:46:21 -0600

    fix: crud de orden y injection de modelo

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,26 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Order } from './schemas/order.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class OrderService {
-  create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order';
+  constructor(
+    @InjectModel(Order.name) private readonly orderModel: Model<Order>,
+  ) {}
+
+  async create(createOrderDto: CreateOrderDto): Promise<Order> {
+    const order = await this.orderModel.create(createOrderDto);
+    return order;
   }
 
-  findAll() {
-    return `This action returns all order`;
+  async findAll(): Promise<Order[]> {
+    return await this.orderModel.find().exec();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} order`;
+  async findOne(id: string): Promise<Order> {
+    const order = await this.orderModel.findById(id).exec();
+    if (!order) throw new NotFoundException(`Orden con id ${id} no encontrada`);
+    return order;
   }
 
-  update(id: number, updateOrderDto: UpdateOrderDto) {
-    return `This action updates a #${id} order`;
+  async update(id: string, updateOrderDto: UpdateOrderDto) {
+    const order = await this.orderModel.findByIdAndUpdate(id, updateOrderDto, {
+      new: true,
+    });
+    if (!order) throw new NotFoundException(`Orden con id ${id} no encontrada`);
+    return order;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} order`;
+  async remove(id: string) {
+    const result = await this.orderModel.findByIdAndDelete(id);
+    if (!result)
+      throw new NotFoundException(`Orden con id ${id} no encontrada`);
   }
 }
