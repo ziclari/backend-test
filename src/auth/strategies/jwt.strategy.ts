cd4bf02ddbad2f61d4c90c81966f1b9aac7441ae
ziclari <citlalli688@outlook.com>
2025-08-30 22:33:07 -0600

    fix: jwt secret ahora es un secret

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,12 +1,17 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { jwtConstants } from '../constants/jwt.constant';
 import { JwtPayload } from '../interfaces/jwt-payload.interface';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
+    if (!jwtConstants.secret) {
+      throw new InternalServerErrorException(
+        'No se ha agregado un secreto para JWT',
+      );
+    }
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
