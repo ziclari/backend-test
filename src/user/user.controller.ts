5862e7d72d6ab8ab8888bce843c7c72bf1a1610c
ziclari <citlalli688@outlook.com>
2025-08-30 22:09:01 -0600

    add: proteccion de rutas crud user y consistencia en module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,11 +6,14 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
 
+@UseGuards(JwtAuthGuard)
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
